352fdcec838f916e0c4c01d8101210526cb9360e
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-04-28 23:41:16 +0900

    Update move.cpp

--- ESP32-PS4/Class/move.cpp
+++ ESP32-PS4/Class/move.cpp
@@ -1,4 +1,15 @@
 #include"move.h"
+// #include<Arduino.h>
+// #include"Arduino.h"
+
+const int motor1Pin1 = 2;
+const int motor1Pin2 = 3;
+const int motor2Pin1 = 4;
+const int motor2Pin2 = 5;
+const int motor3Pin1 = 6;
+const int motor3Pin2 = 7;
+const int motor4Pin1 = 8;
+const int motor4Pin2 = 9;
 
 void move::moveForward(){
 // 前進
@@ -11,4 +22,10 @@ void move::moveLeft(){
 }
 void move::moveBack(){
 // 後進
+}
+void move::turnRight(){
+// 右回転
+}
+void move::turnLeft(){
+// 左回転
 }
