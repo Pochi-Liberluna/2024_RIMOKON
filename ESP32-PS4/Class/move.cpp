814debefb06bc67824f04bfaa1c9dafe600344e3
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-04-29 00:10:54 +0900

    Update move.cpp

--- ESP32-PS4/Class/move.cpp
+++ ESP32-PS4/Class/move.cpp
@@ -2,20 +2,42 @@
 // #include<Arduino.h>
 // #include"Arduino.h"
 
-const int motor1Pin1 = 2;
+const int motor1Pin1 = 2;  //左前
 const int motor1Pin2 = 3;
-const int motor2Pin1 = 4;
+const int motor2Pin1 = 4;  //左後ろ
 const int motor2Pin2 = 5;
-const int motor3Pin1 = 6;
+const int motor3Pin1 = 6;  //右前
 const int motor3Pin2 = 7;
-const int motor4Pin1 = 8;
+const int motor4Pin1 = 8;  //右後ろ
 const int motor4Pin2 = 9;
 
 void move::moveForward(){
-// 前進
+  int LJoyY = PS4.LSticky();
+
+  if(LJoyX >= 200){
+        digitalWrite(motor1Pin1, HIGH);
+        digitalWrite(motor1Pin2, LOW);
+        digitalWrite(motor2Pin1, HIGH);
+        digitalWrite(motor2Pin2, LOW);
+        digitalWrite(motor3Pin1, HIGH);
+        digitalWrite(motor3Pin2, LOW);
+        digitalWrite(motor4Pin1, HIGH);
+        digitalWrite(motor4Pin2, LOW);
+  }
 }
 void move::moveRight(){
-// 右進
+  int LJoyX = PS4.LStickX();
+
+  if(LJoyX >= 200){
+        digitalWrite(motor1Pin1, HIGH);
+        digitalWrite(motor1Pin2, LOW);
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, LOW);
+        digitalWrite(motor3Pin1, LOW);
+        digitalWrite(motor3Pin2, LOW);
+        digitalWrite(motor4Pin1, HIGH);
+        digitalWrite(motor4Pin2, LOW);
+  }
 }
 void move::moveLeft(){
 // 左進
