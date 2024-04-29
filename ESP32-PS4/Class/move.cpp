625fbcf98cf435f79cd38e92720ad1449bef3013
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-04-29 16:47:00 +0900

    Update move.cpp

--- ESP32-PS4/Class/move.cpp
+++ ESP32-PS4/Class/move.cpp
@@ -12,9 +12,6 @@ const int motor4Pin1 = 8;  //右後ろ
 const int motor4Pin2 = 9;
 
 void move::moveForward(){
-  int LJoyY = PS4.LSticky();
-
-  if(LJoyX >= 200){
         digitalWrite(motor1Pin1, HIGH);
         digitalWrite(motor1Pin2, LOW);
         digitalWrite(motor2Pin1, HIGH);
@@ -23,12 +20,8 @@ void move::moveForward(){
         digitalWrite(motor3Pin2, LOW);
         digitalWrite(motor4Pin1, HIGH);
         digitalWrite(motor4Pin2, LOW);
-  }
 }
 void move::moveRight(){
-  int LJoyX = PS4.LStickX();
-
-  if(LJoyX >= 200){
         digitalWrite(motor1Pin1, HIGH);
         digitalWrite(motor1Pin2, LOW);
         digitalWrite(motor2Pin1, LOW);
@@ -37,13 +30,26 @@ void move::moveRight(){
         digitalWrite(motor3Pin2, LOW);
         digitalWrite(motor4Pin1, HIGH);
         digitalWrite(motor4Pin2, LOW);
-  }
 }
 void move::moveLeft(){
-// 左進
+        digitalWrite(motor1Pin1, LOW);
+        digitalWrite(motor1Pin2, LOW);
+        digitalWrite(motor2Pin1, HIGH);
+        digitalWrite(motor2Pin2, LOW);
+        digitalWrite(motor3Pin1, HIGH);
+        digitalWrite(motor3Pin2, LOW);
+        digitalWrite(motor4Pin1, LOW);
+        digitalWrite(motor4Pin2, LOW);
 }
 void move::moveBack(){
-// 後進
+        digitalWrite(motor1Pin1, LOW);
+        digitalWrite(motor1Pin2, HIGH);
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, HIGH);
+        digitalWrite(motor3Pin1, LOW);
+        digitalWrite(motor3Pin2, HIGH);
+        digitalWrite(motor4Pin1, LOW);
+        digitalWrite(motor4Pin2, HIGH);
 }
 void move::turnRight(){
 // 右回転
