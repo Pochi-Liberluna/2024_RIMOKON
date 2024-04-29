9a1fb8424205700510390025fed8d4cfa560cb88
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-04-29 21:59:57 +0900

    Update ESP32-PS4.ino

--- ESP32-PS4/ESP32-PS4.ino
+++ ESP32-PS4/ESP32-PS4.ino
@@ -1,12 +1,14 @@
 /*ESP32に記述*/
 #include<ps4.h>
 #include<PS4Controller.h>
+// #include<SPI.h>
 
 #include"../Class/move.h"
 
 // #define
 
-  move DC_Motor;
+ /* PCA9685 pwm = PCA9685(0x40);
+  move DC_Motor; */
 
 void setup(){
   Serial.begin(9600);
@@ -17,6 +19,9 @@ void setup(){
 
   PS4.begin("1a:2b:3c:01:01:01"); //MACアドレス
   Serial.printf("Ready.");
+
+ /* pwm.begin();
+  pwm.setPWMFreq(50); //PWM周期を50Hzに設定 */
 
   // DC_Motor.speed = 100;
 }
@@ -33,33 +38,33 @@ void loop(){
     Serial.printf("Connected.\n");
     //Serial.printf("Battery Level : %d\n", PS4.Battery());
 
-    if(PS4.Right()) Serial.println("Right Button\n");
-    if(PS4.Left()) Serial.println("Left Button\n");
-    if(PS4.Up()) Serial.println("Up Button\n");
-    if(PS4.Down()) Serial.println("Down Button\n");
+    if(PS4.Right()) Serial.write('a');
+    if(PS4.Left()) Serial.write('b');
+    if(PS4.Up()) Serial.write('c');
+    if(PS4.Down()) Serial.write('d');
 
-    if(PS4.Square()) Serial.println("Square Button\n"); //□
-    if(PS4.Cross()) Serial.println("Cross Button\n"); //×
-    if(PS4.Circle()) Serial.println("Circle Button\n"); //○
-    if(PS4.Triangle()) Serial.println("Triangle Button\n"); //△
+    if(PS4.Square()) Serial.write('e'); //□
+    if(PS4.Cross()) Serial.write('f'); //×
+    if(PS4.Circle()) Serial.write('g'); //○
+    if(PS4.Triangle()) Serial.write('h'); //△
 
-    if(PS4.UpRight()) Serial.println("UpRight Button\n");
-    if(PS4.DownRight()) Serial.println("DownRight Button\n");
-    if(PS4.UpLeft()) Serial.println("UpLeft Button\n");
-    if(PS4.DownLeft()) Serial.println("DownLeft Button\n");
+    if(PS4.UpRight()) Serial.write('i');
+    if(PS4.DownRight()) Serial.write('j');
+    if(PS4.UpLeft()) Serial.write('k');
+    if(PS4.DownLeft()) Serial.write('l');
 
-    if(PS4.L1()) Serial.println("L1 Button\n");
-    if(PS4.R1()) Serial.println("R1 Button\n");
+    if(PS4.L1()) Serial.write('m');
+    if(PS4.R1()) Serial.write('n');
 
-    if(PS4.Share()) Serial.println("Share Button\n");
-    if(PS4.Options()) Serial.println("Options Button\n");
-    if(PS4.L3()) Serial.println("L3 Button\n");
-    if(PS4.R3()) Serial.println("R3 Button\n");
+    if(PS4.Share()) Serial.write('o');
+    if(PS4.Options()) Serial.write('p');
+    if(PS4.L3()) Serial. write('q');
+    if(PS4.R3()) Serial.write('r');
 
-    if(PS4.PSButton()) Serial.println("PS Button\n");
-    if(PS4.Touchpad()) Serial.println("Touch Pad\n");
+    if(PS4.PSButton()) Serial.write('s');
+    if(PS4.Touchpad()) Serial.write('t');
 
-      if(PS4.L2()){
+  /*  if(PS4.L2()){
        Serial.printf("L2 button at %d\n", PS4.L2Value());
       }
       if(PS4.R2()){
@@ -76,15 +81,17 @@ void loop(){
       }
       if(PS4.RStickY()){
         Serial.printf("Righr Stick Y at %d\n", PS4.RStickY());
-      }
+      }  */
 
       Serial.println();
       // delay(10);
   }
   if(LJoyY >= 175){
-    DC_Mortor.moveForward();
+   // DC_Mortor.moveForward();
+   Serial.write('0');
   }
   if(LJoyY =< 75){
-    DC_Motor.moveBack();
+   // DC_Motor.moveBack();
+   Serial.write('1');
   }
 }
