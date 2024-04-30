25f7cd9335c8e597fce41cabcc961dde397614a5
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-04-30 11:29:11 +0900

    Update Arm.h

--- ESP32-PS4/Class/Arm.h
+++ ESP32-PS4/Class/Arm.h
@@ -3,5 +3,12 @@
 
 class {
 public:
-
+  void Arm1_open();
+  void Arm1_close();
+  void Arm2_open();
+  void Arm2_close();
+  void Arm3_open();
+  void Arm3_close();
+  void Arm4_open();
+  void Arm4_close();
 }
