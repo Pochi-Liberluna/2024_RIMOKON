a16ae1bd141a68e6276c61da3fdc4f2c44a54701
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-04-30 11:35:12 +0900

    Create Arm.cpp

---
+++ ESP32-PS4/Class/Arm.cpp
@@ -0,0 +1,26 @@
+#include"Arm.h"
+
+void Arm1_open(){
+  
+}
+void Arm1_close(){
+  
+}
+void Arm2_open(){
+  
+}
+void Arm2_close(){
+  
+}
+void Arm3_open(){
+  
+}
+void Arm3_close(){
+  
+}
+void Arm4_open(){
+  
+}
+void Arm4_close(){
+  
+}
