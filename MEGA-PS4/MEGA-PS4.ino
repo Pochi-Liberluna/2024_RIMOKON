6034594d542f10e99314dc5cd9fecb6a6906f788
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-04-30 09:35:05 +0900

    Update MEGA-PS4.ino

--- MEGA-PS4/MEGA-PS4.ino
+++ MEGA-PS4/MEGA-PS4.ino
@@ -1,3 +1,12 @@
+/** Serial.read()用
+a = PS4.Right();  b = PS4.Left();  c = PS4.Up();  d = PS4.Down();
+e = PS4.Square();  f = PS4.Cross();  g = PS4.Circle();  h = PS4.Triangle();
+i = PS4.UpRight();  j = PS4.DownRight();  k = PS4.UpLeft();  l = PS4.DownLeft();
+m = PS4.L1();  n = PS4.R1();  o = PS4.L2();  p = PS4.R2();
+q = PS4.Share();  r = PS4.Options();  s = PS4.L3();  t = PS4.R3();
+u = PS4.PSButton();  v = PS4.Touchpad();
+0 = moveForward();  1 = moveBack();  2 = moveRight();  3 = moveLeft();
+**/
 #include"../../ESP32-PS4/Class/move.h"
 
 #include<PCA9685.h>
