abfadc6b5b3a61a250551964716fa123c3342275
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-05-28 12:04:11 +0900

    Update DC_PWM.ino

--- Test/DC_PWM.ino
+++ Test/DC_PWM.ino
@@ -1,19 +1,59 @@
 int speed = "255";
 
+// MEGA を使う場合のPWMピン
+  int RF_R = "2";
+  int LF_R = "3";
+  int RB_R = "4";
+  int LB_R = "5";
+  int RF_L = "6";
+  int LF_L = "7";
+  int RB_L = "8";
+  int LB_L = "9";
+
 void setup(){
   Serial.begin(115200);
 }
 
+void moveForward(){
+  analogWrigte(RF_R, speed);
+  analogWrigte(LF_R, speed);
+  analogWrigte(RB_R, speed);
+  analogWrigte(LB_R, speed);
+}
+void moveBack(){
+  analogWrigte(RF_L, speed);
+  analogWrigte(LF_L, speed);
+  analogWrigte(RB_L, speed);
+  analogWrigte(LB_L, speed);
+}
+void moveLeft(){
+  analogWrigte(RF_R, speed);
+  analogWrigte(LF_L, speed);
+  analogWrigte(RB_L, speed);
+  analogWrigte(LB_R, speed);
+}
+void moveRight(){
+  analogWrigte(RF_L, speed);
+  analogWrigte(LF_R, speed);
+  analogWrigte(RB_R, speed);
+  analogWrigte(LB_L, speed);
+}
+
 void loop(){
 
-  int RF_R = "";
-  int LF_R = "";
-  int Rı_R = "";
-  int LB_R = "";
-  int RF_L = "";
-  int LF_L = "";
-  int RB_L = "";
-  int LB_L = "";
+  moveForward();
+  
+  delay(100);
 
-  analogWrite();
+  moveBack();
+
+  delay(100);
+
+  moveRight();
+
+  delay(100);
+
+  moveLeft();
+
+  delay(100);
 }
