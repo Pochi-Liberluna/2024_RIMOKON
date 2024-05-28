51ea0706038fb4200be95ea363d9f61979d6708a
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-05-28 10:57:49 +0900

    Update DC_PWM.ino

--- Test/DC_PWM.ino
+++ Test/DC_PWM.ino
@@ -1,8 +1,19 @@
+int speed = "255";
 
 void setup(){
-  
+  Serial.begin(115200);
 }
 
 void loop(){
-  
+
+  int RF_R = "";
+  int LF_R = "";
+  int Rı_R = "";
+  int LB_R = "";
+  int RF_L = "";
+  int LF_L = "";
+  int RB_L = "";
+  int LB_L = "";
+
+  analogWrite();
 }
