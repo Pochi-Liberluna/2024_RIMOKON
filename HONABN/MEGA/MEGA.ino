dc7a38d3abeb8a13e6770643dd8a4397cf521125
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-08-29 21:24:12 +0900

    Update MEGA.ino

--- HONABN/MEGA/MEGA.ino
+++ HONABN/MEGA/MEGA.ino
@@ -3,10 +3,18 @@
 
 #include"./../Class/move.h"
 
+Servo Servo1, Servo2, Servo3, Servo4;
+
 void setup(){
-  
+  Serial.begin(115200);
+  Servo1.attach();
+  Servo2.attach();
+  Servo3.attach();
+  Servo4.attach();
 }
 
 void loop(){
-  
+  if(Serial.available() > 0){
+    char ;
+  }
 }
